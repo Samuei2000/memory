0e61117f6bd7b471073dc5071c887f0f68a52d80
Samuel Song <kyohn9178@gmail.com>
2023-06-29 18:31:50 +0100

    update Memory validations

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -2,4 +2,12 @@ defmodule PensieveWeb.MemoryHTML do
 
   use PensieveWeb, :html
   embed_templates "/memory_html/*"
+
+  alias Pensieve.Memories.Memory
+
+  def memory_inserted_at(%Memory{inserted_at: timestamp}) do
+
+    Calendar.strftime(timestamp, "%m/%d/%Y %I:%M%p")
+
+  end
 end
