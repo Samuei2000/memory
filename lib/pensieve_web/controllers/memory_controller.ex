42af5862144b7e089f95169ed5ff1afdcd08b538
Samuel Song <kyohn9178@gmail.com>
2023-06-29 19:21:22 +0100

    Add 'new' and 'create' actions

--- lib/pensieve_web/controllers/memory_controller.ex
+++ lib/pensieve_web/controllers/memory_controller.ex
@@ -3,6 +3,7 @@ defmodule PensieveWeb.MemoryController do
   use PensieveWeb, :controller
 
   alias Pensieve.Memories
+  alias Pensieve.Memories.Memory
 
 
   def index(conn, _params) do
@@ -13,5 +14,23 @@ defmodule PensieveWeb.MemoryController do
   def show(conn, %{"id" => id}) do
     memory = Memories.get_memory!(id)
     render(conn, :show, memory: memory)
+  end
+
+  def new(conn, _params) do
+    changeset = Memories.change_memory(%Memory{})
+    render(conn, :new, changeset: changeset)
+  end
+
+  def create(conn, %{"memory" => memory_params}) do
+
+    case Memories.create_memory(memory_params) do
+      {:ok, memory} ->
+        conn
+        |> put_flash(:info, "Memory created successfully.")
+        |> redirect(to: ~p"/memories/#{memory}")
+      {:error, %Ecto.Changeset{} = changeset} ->
+        IO.inspect(changeset)
+        render(conn, :new, changeset: changeset)
+    end
   end
 end
